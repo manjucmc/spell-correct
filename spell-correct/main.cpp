927a32ba69a40ab4603c3f82e733d3536b6e075d
manjucmc <manju_cmc@yahoo.com>
2015-01-17 10:05:32 -0800

    Initial Commit

---
+++ spell-correct/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  spell-correct
+//
+//  Created by Manju Balasubramaniam on 1/17/15.
+//  Copyright (c) 2015 Manju Balasubramaniam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
